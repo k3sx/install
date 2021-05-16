ab3957e665081de106d950b36f1e28a6ae67e5a0
k3sx <k3sx@pm.me>
2021-05-16 23:34:22 +0200

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # Front Matter
 set -x
+set -o vi
 swapoff -a
 
 # k3s
@@ -13,7 +14,7 @@ echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> $HOME/.bashrc
 
 # Helm
 curl -sfL https://get.helm.sh/helm-v3.5.4-linux-amd64.tar.gz | tar -xzvf -
-mv linux-amd64/helm /usr/local/bin
+mv linux-amd64/helm /usr/local/bin && rm -r linux-amd64
 
 # Apps
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
@@ -23,3 +24,9 @@ kubectl apply -f https://sunstone.dev/keel?tag=0.17.0-rc1
 curl -sfL https://github.com/derailed/k9s/releases/download/v0.24.9/k9s_Linux_x86_64.tar.gz \
   | tar -xzvf -
 mv k9s /usr/local/bin
+
+# minimal-vimrc
+git clone https://github.com/k3sx/minimal-vimrc.git
+mv minimal-vimrc/.vimrc . && rm -r minimal-vimrc
+
+cat $KUBECONFIG
