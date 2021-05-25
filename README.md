64180b2cf069751085ba83f8eb6520a7cdf8f4e0
k3sx <k3sx@pm.me>
2021-05-25 13:44:27 +0200

    add multi-install

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # install
+
+`curl -sfL https://raw.githubusercontent.com/k3sx/install/main/install.sh | sh -`
---
+++ mi.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Front Matter
+set -x
+set -o vi
+swapoff -a
+
+# k3s
+if (( $# < 2 )); then
+  embed=" --token $1 --cluster-init"
+else
+  embed=" --token $1 --server $2"
+fi
+
+command="curl -sfL https://get.k3s.io | \
+  INSTALL_K3S_CHANNEL=latest \
+  INSTALL_K3S_VERSION=v1.21.0+k3s1 \
+  sh -s - \
+  --disable traefik \
+  ${embed}"
+
+eval "$command"
+
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> $HOME/.bashrc 
+. $HOME/.bashrc
+
+# Helm
+curl -sfL https://get.helm.sh/helm-v3.5.4-linux-amd64.tar.gz | tar -xzvf -
+mv linux-amd64/helm /usr/local/bin && rm -r linux-amd64
+
+# k9s
+curl -sfL https://github.com/derailed/k9s/releases/download/v0.24.9/k9s_Linux_x86_64.tar.gz \
+  | tar -xzvf -
+mv k9s /usr/local/bin
+
+# minimal-vimrc
+git clone https://github.com/k3sx/minimal-vimrc.git
+mv minimal-vimrc/.vimrc . && rm -r minimal-vimrc
+
+cat $KUBECONFIG
